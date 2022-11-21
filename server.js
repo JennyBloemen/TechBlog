38b2964b70d65b1cfab795a42baf5a0184d4cfb9
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-21 11:35:55 -0700

    File setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+// const helpers = require('./utils/helpers');
+
+const sequelize = require("./config/connection");
+
+// Create a new sequelize store using the express-session package
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// const hbs = exphbs.create({ helpers });
+
+// Configure and link a session object with the sequelize store
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
